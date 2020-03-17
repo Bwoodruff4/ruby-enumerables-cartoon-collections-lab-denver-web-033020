da0b08957c9bb63bcd33e7cf445d5c1f4530e84f
Blake <blakewoodruff4@gmail.com>
2020-03-17 18:48:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(calls)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
+  
 end
 
 def find_the_cheese# code an argument here
